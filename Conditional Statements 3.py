0cf94cebcde037c24a43140517013085158e1c2f
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-06-01 14:42:00 -0400

    Add files via upload

---
+++ Conditional Statements 3.py
@@ -0,0 +1,39 @@
+# if = Do some code only IF some conditions is True
+#      Else do something else
+
+# age = int(input("Enter your age here:"))
+#
+# if age >= 18:
+#     print("You can sign up")
+# elif age < 0:
+#     print("You have not been born yet")
+# elif age <= 17:
+#     print("You are too young to sign up")
+# else:
+#     print("Invalid input")
+#
+#
+# response = input("Would you like some food? (Y/N): ")
+#
+# if response == "Y":
+#     print("You are very hungry then")
+# elif response == "N":
+#     print("You are not hungry I guess")
+# else:
+#     print("Invalid input")
+#
+
+# name = input("Enter your name: ")
+#
+# if name == "":
+#     print("Sorry you did not input anything, try again")
+# else:
+#     print(f"Hello, {name}")
+
+for_sale = False
+
+if for_sale:
+    print("The item is for sale")
+else:
+    print("The item is not for sale")
+
